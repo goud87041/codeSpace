31f8e3e873d3af06ae1541fd6bad5319eb44fcf9
goud87041 <152836840+goud87041@users.noreply.github.com>
2026-06-24 16:15:05 +0000

    Add user model schema with validation for username, email, and password

---
+++ authentication/backend/src/model/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+
+    userName :{
+        required : true ,
+        type : String 
+    },
+    email: {
+        required : true ,
+        type : String 
+    },
+    password : {
+        required : true ,
+        type : String
+    }
+
+},{timestamps:true});
+
+const User = mongoose.model('User', userSchema);
