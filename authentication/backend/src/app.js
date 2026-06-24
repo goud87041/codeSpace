2c1c210fd518b428fc60829fd169969a11f97a63
goud87041 <152836840+goud87041@users.noreply.github.com>
2026-06-24 10:44:26 +0000

    push code

---
+++ authentication/backend/src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the backend connection API!' });
+});
+
+export default app;
+
+
+
+
---
+++ authentication/backend/src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import app from './app.js';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+// import connectDB from 'mongoose';
+
+connectDB();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT , () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+
