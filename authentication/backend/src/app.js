bbd2ce30873bdf3b5bfedabd16eafcd94ebf3a5f
goud87041 <152836840+goud87041@users.noreply.github.com>
2026-06-30 12:15:11 +0000

    Add health route for connection status check

--- authentication/backend/src/app.js
+++ authentication/backend/src/app.js
@@ -1,19 +1,15 @@
 import express from 'express';
 import cors from 'cors';
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import userRoute from "./routes/user.route.js"
 import bookRoute from "./routes/book.route.js"
 import healthRoute from "./routes/health.route.js"
-=======
+
 import {userRoute} from "./routes/user.route"
 import {bookRoute} from "./routes/book.route"
-=======
+
 import userRoute from "./routes/user.route.js"
 import bookRoute from "./routes/book.route.js"
->>>>>>> 20a69a4 (Fix import paths in controllers and routes; update to use .js extension)
-
->>>>>>> 6198316 (Implement user and book routes with JWT authentication; add user registration and login functionality)
 
 const app = express();
 
@@ -27,10 +23,8 @@ app.get('/', (req, res) => {
 
 app.use("/api/user", userRoute);
 app.use("/api/Book" , bookRoute);
-<<<<<<< HEAD
 app.use("/api/health", healthRoute)
-=======
->>>>>>> 6198316 (Implement user and book routes with JWT authentication; add user registration and login functionality)
+app.use("/api/health", healthRoute)
 
 
 
