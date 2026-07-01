a44cda9a9d9278769832b096285c6fd487bd67f3
goud87041 <152836840+goud87041@users.noreply.github.com>
2026-07-01 11:06:04 +0000

    Refactor code structure and improve formatting across multiple files; update package-lock.json, controllers, models, and routes for consistency and clarity

--- authentication/backend/src/model/book.model.js
+++ authentication/backend/src/model/book.model.js
@@ -1,38 +1,33 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const bookSchema = new mongoose.Schema({
-    title : {
-        type : String ,
-        required : true 
+const bookSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
     },
-    author : {
-        type : String ,
-        required : true
-    },
-    description : {
-        type : String ,
-        required : true
-    },
-    price : {
-        type : Number ,
-        required : true
-    },
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6198316 (Implement user and book routes with JWT authentication; add user registration and login functionality)
-    user : {
-        type : mongoose.Schema.Types.ObjectId ,
-        ref : 'User'
+    {
+        timestamps: true,
     }
-<<<<<<< HEAD
-=======
->>>>>>> 3abe013 (Add book model and controller with CRUD operations)
-=======
->>>>>>> 6198316 (Implement user and book routes with JWT authentication; add user registration and login functionality)
-},{
-    timestamps : true
-})
+);
 
-const Book = mongoose.model('Book', bookSchema);
+const Book = mongoose.model("Book", bookSchema);
+
 export default Book;
